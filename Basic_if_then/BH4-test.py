442c0a58ed20f2fda28c107420d02a62d9cfab04
BH4 <brycefore4@gmail.com>
2018-10-18 00:27:14 -0700

    Added decision functions and parameters for easier reading
    Also fixed bug where ships on new droppoints wouldn't move.

--- Basic_if_then/BH4-test.py
+++ Basic_if_then/BH4-test.py
@@ -6,13 +6,14 @@ from hlt.positionals import Direction, Position
 import random
 import logging
 
-move_cost_fraction = .1
 
-
+###############################################################################
+# Movement functions
+###############################################################################
 def random_move(ship, game_map, occupied):
     curr_pos = ship.position
 
-    move_cost = move_cost_fraction*game_map[curr_pos].halite_amount
+    move_cost = game_map[curr_pos].halite_amount/constants.MOVE_COST_RATIO
 
     move = Direction.Still
     new_pos = curr_pos
@@ -38,7 +39,7 @@ def random_move(ship, game_map, occupied):
 def returning_move(ship, game_map, occupied):
     curr_pos = ship.position
 
-    move_cost = move_cost_fraction*game_map[curr_pos].halite_amount
+    move_cost = game_map[curr_pos].halite_amount/constants.MOVE_COST_RATIO
 
     move = Direction.Still
     new_pos = curr_pos
@@ -66,10 +67,49 @@ def closest_drop(ship, me, game_map):
     return closest, dist
 
 
-game = hlt.Game()
+###############################################################################
+# Decision functions
+###############################################################################
+def i_should_make_a_dropoff(dist, game, ship):
+    me = game.me
+    game_map = game.game_map
 
+    too_far = dist > large_distance_from_drop
+    sufficent_num_ships = len(me.get_ships()) > 1
+    sufficient_halite_to_build = (me.halite_amount > constants.DROPOFF_COST-game_map[ship.position].halite_amount)
+    not_end_game = game.turn_number < turn_to_stop_spending
+
+    return (too_far and sufficent_num_ships and
+            sufficient_halite_to_build and not_end_game)
+
+
+def i_should_spawn_ship(game, occupied):
+    me = game.me
+
+    not_end_game = game.turn_number < turn_to_stop_spending
+    sufficient_halite_to_build = me.halite_amount >= constants.SHIP_COST
+    not_busy = me.shipyard.position not in occupied
+    need_ships = len(me.get_ships()) < max_ships
+    lots_of_halite = (me.halite_amount >
+                      constants.DROPOFF_COST+constants.SHIP_COST)
+
+    return (not_end_game and sufficient_halite_to_build and not_busy and
+            (need_ships or lots_of_halite))
+
+
+###############################################################################
+# Game
+###############################################################################
+game = hlt.Game()
 # Pre-processing area
 ship_status = {}
+
+# Game parameters
+max_ships = 10
+turn_to_stop_spending = 300
+large_distance_from_drop = 10
+minimum_useful_halite = constants.MAX_HALITE/10
+sufficient_halite_for_droping = constants.MAX_HALITE/4
 
 
 game.ready("BH4-test")
@@ -92,16 +132,18 @@ while True:
         if ship.id not in ship_status:
             ship_status[ship.id] = "exploring"
 
-        if ship_status[ship.id] == "returning" and ship.position == me.shipyard.position:
-            ship_status[ship.id] = "exploring"
-        elif ship.halite_amount >= constants.MAX_HALITE/4:
+        if ship_status[ship.id] == "returning":
+            closest, dist = closest_drop(ship, me, game_map)
+            if dist == 0:
+                ship_status[ship.id] = "exploring"
+        elif ship.halite_amount >= sufficient_halite_for_droping:
             ship_status[ship.id] = "returning"
 
         # Take action
         if ship_status[ship.id] == "returning":
             closest, dist = closest_drop(ship, me, game_map)
 
-            if dist > 7 and len(me.get_ships()) > 1 and me.halite_amount > constants.DROPOFF_COST:
+            if i_should_make_a_dropoff(dist, game, ship):
                 command_queue.append(ship.make_dropoff())
 
                 logging.info("Ship {} decided to make a dropoff.".format(ship.id))
@@ -111,7 +153,7 @@ while True:
 
                 logging.info("Ship {} is returning by moving {}.".format(ship.id, move))
         elif ship_status[ship.id] == "exploring":
-            if game_map[ship.position].halite_amount < constants.MAX_HALITE/10:
+            if game_map[ship.position].halite_amount < minimum_useful_halite:
                 move, occupied = random_move(ship, game_map, occupied)
                 command_queue.append(ship.move(move))
 
@@ -124,11 +166,7 @@ while True:
 
     # If you're on the first turn and have enough halite, spawn a ship.
     # Don't spawn a ship if you currently have a ship at port, though.
-    if ((game.turn_number <= 300 and me.halite_amount >= constants.SHIP_COST
-         and me.shipyard.position not in occupied and
-         (len(me.get_ships()) < 4 or
-          me.halite_amount > constants.DROPOFF_COST+constants.SHIP_COST))):
-
+    if i_should_spawn_ship(game, occupied):
         command_queue.append(game.me.shipyard.spawn())
 
     # Send your moves back to the game environment, ending this turn.
