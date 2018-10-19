f5286f5b7a143a8f8c47b9d784ce0fd054c83cc4
BH4 <brycefore4@gmail.com>
2018-10-18 23:45:02 -0700

    Removed occupied array in favor of is_occupied map variable
    Switched from self made occupation array to build in methods.
    Also applied a sort of quick fix to smart_explore.  Needs more testing.

--- quick_game_test/run_game.bat
+++ quick_game_test/run_game.bat
@@ -1,1 +1,1 @@
-halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "python ../Basic_if_then/test-smart-explore.py" "python ../Basic_if_then/BH4-test.py"
+halite.exe --replay-directory replays/ -vvv --width 32 --height 32 "python ../Basic_if_then/BH4-test.py" "python working-BH4-test.py"
