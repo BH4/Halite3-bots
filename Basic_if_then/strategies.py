1a0b40939cf974bc9e1e1ccb911e1fd21b2099e4
BH4 <brycefore4@gmail.com>
2018-10-21 10:27:33 -0700

    Make "dropoff" its own action
    making a dropoff can now be done independent of returning.
    Closes #4

--- Basic_if_then/strategies.py
+++ Basic_if_then/strategies.py
@@ -18,32 +18,35 @@ def expand(game, ship_status, params):
         game_map[ship.position].mark_unsafe(ship)
 
     for ship in me.get_ships():
+        # Pre-computed values for this ship
+        closest, dist = helpers.closest_drop(ship, me, game_map)
+
+        #######################################################################
         # Mark status
+        #######################################################################
         if ship.id not in ship_status:
             ship_status[ship.id] = "exploring"
 
-        if ship_status[ship.id] == "returning":
-            closest, dist = helpers.closest_drop(ship, me, game_map)
-            if dist == 0:
-                ship_status[ship.id] = "exploring"
-        elif ship.halite_amount >= params.sufficient_halite_for_droping:
+        # Status changes
+        if ship_status[ship.id] == "returning" and dist == 0:
+            ship_status[ship.id] = "exploring"
+        elif (ship_status[ship.id] == "exploring" and
+              ship.halite_amount >= params.sufficient_halite_for_droping):
             ship_status[ship.id] = "returning"
 
+        if checklists.dropoff_checklist(dist, game, ship, ship_status, params):
+            ship_status[ship.id] = "dropoff"
+
+            logging.info("Ship {} decided to make a dropoff.".format(ship.id))
+
+        #######################################################################
         # Take action
+        #######################################################################
         if ship_status[ship.id] == "returning":
-            closest, dist = helpers.closest_drop(ship, me, game_map)
+            move = movement.returning_move(ship, game_map, closest)
+            command_queue.append(ship.move(move))
 
-            if checklists.dropoff_checklist(dist, game, ship, ship_status, params):
-                command_queue.append(ship.make_dropoff())
-                ship_status[ship.id] = "dropoff"
-
-                logging.info("Ship {} decided to make a dropoff.".format(ship.id))
-                logging.info("output ship statuses to make sure only one is making dropoff: " + str(ship_status))
-            else:
-                move = movement.returning_move(ship, game_map, closest)
-                command_queue.append(ship.move(move))
-
-                logging.info("Ship {} is returning by moving {}.".format(ship.id, move))
+            logging.info("Ship {} is returning by moving {}.".format(ship.id, move))
         elif ship_status[ship.id] == "exploring":
             if game_map[ship.position].halite_amount < params.minimum_useful_halite:
                 move = movement.smart_explore(ship, game_map)
@@ -54,14 +57,21 @@ def expand(game, ship_status, params):
                 command_queue.append(ship.stay_still())
 
                 logging.info("Ship {} is collecting by staying still.".format(ship.id))
+        elif ship_status[ship.id] == "dropoff":
+            command_queue.append(ship.make_dropoff())
 
-    # If you're on the first turn and have enough halite, spawn a ship.
-    # Don't spawn a ship if you currently have a ship at port, though.
+    #######################################################################
+    # Ship spawn
+    #######################################################################
     if checklists.ship_spawn_checklist(game, ship_status, params):
         command_queue.append(game.me.shipyard.spawn())
 
+    #######################################################################
     # Reset turn based variables
+    #######################################################################
     helpers.stored_density = None
 
+    #######################################################################
     # Send your moves back to the game environment, ending this turn.
+    #######################################################################
     game.end_turn(command_queue)
