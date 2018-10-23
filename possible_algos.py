3ff837be877cc40016944c081c300aeaa848ef57
BH4 <brycefore4@gmail.com>
2018-10-23 10:33:45 -0700

    add possible_algos

---
+++ possible_algos.py
@@ -0,0 +1,16 @@
+Tree Search:
+    uninformed search
+    best-first search
+    minimax
+    monte carlo tree search
+Evolutionary computation:
+    Local Search
+    Evolutionary algorithms
+Supervised learning:
+    support vector machine
+    neural network
+    decision tree learning
+Reinforcement learning:
+    Q-learning
+Unsupervised learning:
+    clustering
